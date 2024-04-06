5a884425f41dc3a8fcd831cd670d0679e0e032cd
Александр Аникин <walkingkowboy@gmail.com>
2024-04-06 23:43:07 +0300

    Рефакторинг

--- src/service/history/HistoryManager.java
+++ src/service/history/HistoryManager.java
@@ -5,7 +5,9 @@ import model.Task;
 import java.util.List;
 
 public interface HistoryManager {
-void addTaskHistory(Task task);
- void remove(int id);
- List<Task> getHistory();
+    void addTaskHistory(Task task);
+
+    void remove(int id);
+
+    List<Task> getHistory();
 }
