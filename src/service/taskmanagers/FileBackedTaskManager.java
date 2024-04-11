4647d53475995c0ff671f936c57caf6ba5dee3b3
Александр Аникин <walkingkowboy@gmail.com>
2024-04-11 23:50:59 +0300

    Рефакторинг

--- src/service/taskmanagers/FileBackedTaskManager.java
+++ src/service/taskmanagers/FileBackedTaskManager.java
@@ -14,7 +14,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
